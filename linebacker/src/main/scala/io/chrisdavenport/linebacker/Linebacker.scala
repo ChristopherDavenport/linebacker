1991a719b10e9ca27ab22c61dec17411a6b0169b
Christopher Davenport <Chris@ChristopherDavenport.tech>
2018-05-30 21:17:04 -0400

    Add blockTimer and rename to blockShift

--- linebacker/src/main/scala/io/chrisdavenport/linebacker/Linebacker.scala
+++ linebacker/src/main/scala/io/chrisdavenport/linebacker/Linebacker.scala
@@ -2,7 +2,7 @@ package io.chrisdavenport.linebacker
 
 import cats._
 import cats.implicits._
-import cats.effect.Async
+import cats.effect._
 import java.util.concurrent.ExecutorService
 import scala.concurrent.ExecutionContext
 
@@ -10,16 +10,32 @@ trait Linebacker[F[_]] {
 
   def blockingPool: ExecutionContext
 
+  // TODO: REMOVE 0.2
+  final def block[A](fa: F[A])(implicit F: Async[F], ec: ExecutionContext): F[A] =
+    blockShift(fa)(F, ec)
+
   /**
    * Attempts to Run the Given `F[A]` on the blocking pool.
    * Then shifts back to the given implicit execution context
    * after the Async `F[A]` is evaluated.
    */
-  final def block[A](fa: F[A])(implicit F: Async[F], ec: ExecutionContext): F[A] =
+  final def blockShift[A](fa: F[A])(implicit F: Async[F], ec: ExecutionContext): F[A] =
     for {
       _ <- Async.shift(blockingPool)
       eA <- fa.attempt
       _ <- Async.shift(ec)
+      a <- Applicative[F].pure(eA).rethrow
+    } yield a
+
+  /**
+   * Attempts to Run the Given `F[A]` on the blocking pool.
+   * Then shifts back to the F for the timer.
+   */
+  final def blockTimer[A](fa: F[A])(implicit F: Async[F], timer: Timer[F]): F[A] =
+    for {
+      _ <- Async.shift(blockingPool)
+      eA <- fa.attempt
+      _ <- timer.shift
       a <- Applicative[F].pure(eA).rethrow
     } yield a
 }
