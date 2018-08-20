c75b92f0845a4dd6b9d50b69888d6751346c7a58
Jakub Kozłowski <kubukoz@gmail.com>
2018-08-19 20:12:40 -0700

    WIP make thread factories configurable

--- linebacker/src/main/scala/io/chrisdavenport/linebacker/contexts/Executors.scala
+++ linebacker/src/main/scala/io/chrisdavenport/linebacker/contexts/Executors.scala
@@ -1,9 +1,11 @@
 package io.chrisdavenport.linebacker.contexts
 
+import java.util.concurrent.ForkJoinPool.ForkJoinWorkerThreadFactory
+
 import cats.effect.{Resource, Sync}
 import cats.implicits._
 import java.util.concurrent.atomic.AtomicLong
-import java.util.concurrent.{ExecutorService, Executors => E, ForkJoinPool, ThreadFactory}
+import java.util.concurrent.{ExecutorService, ForkJoinPool, ThreadFactory, Executors => E}
 
 object Executors {
 
@@ -17,7 +19,11 @@ object Executors {
    * resource with the same number of threads.
    */
   def fixedPool[F[_]: Sync](n: Int): Resource[F, ExecutorService] =
-    executorServiceResource(fixedPoolExecutorUnsafe(n))
+    fixedPoolC(n)(identity)
+
+  def fixedPoolC[F[_]: Sync](n: Int)(
+      configureThreadFactory: F[ThreadFactory] => F[ThreadFactory]): Resource[F, ExecutorService] =
+    executorServiceResource(fixedPoolExecutorUnsafe(n, configureThreadFactory))
 
   /**
    * Constructs an unbound thread pool that will create
@@ -26,7 +32,11 @@ object Executors {
    * self-manages the number of threads you can consume.
    */
   def unbound[F[_]: Sync]: Resource[F, ExecutorService] =
-    executorServiceResource(unboundExecutorUnsafe)
+    unboundC(identity)
+
+  def unboundC[F[_]: Sync](
+      configureThreadFactory: F[ThreadFactory] => F[ThreadFactory]): Resource[F, ExecutorService] =
+    executorServiceResource(unboundExecutorUnsafe(configureThreadFactory))
 
   /**
    * A work stealing pool is often a useful blocking
@@ -38,39 +48,91 @@ object Executors {
    * requests or other work.
    */
   def workStealingPool[F[_]: Sync](n: Int): Resource[F, ExecutorService] =
-    executorServiceResource(workStealingPoolUnsafe(n))
+    workStealingPoolC(n)(identity)
+
+  def workStealingPoolC[F[_]: Sync](n: Int)(
+      configureThreadFactory: F[ForkJoinWorkerThreadFactory] => F[ForkJoinWorkerThreadFactory]): Resource[
+    F,
+    ExecutorService] =
+    executorServiceResource(workStealingPoolUnsafe(n, configureThreadFactory))
 
   /**
    * Default Pool For Scala, optimized for forked work and then returning to a
    * main pool, generally ideal for your main event loop.
    */
   def forkJoinPool[F[_]: Sync](n: Int): Resource[F, ExecutorService] =
-    executorServiceResource(forkJoinPoolUnsafe(n))
+    forkJoinPoolC(n)(identity)
+
+  def forkJoinPoolC[F[_]: Sync](n: Int)(
+      configureThreadFactory: F[ForkJoinWorkerThreadFactory] => F[ForkJoinWorkerThreadFactory]): Resource[
+    F,
+    ExecutorService] =
+    executorServiceResource(forkJoinPoolUnsafe(n, configureThreadFactory))
 
   private def executorServiceResource[F[_]: Sync](
       f: F[ExecutorService]): Resource[F, ExecutorService] =
     Resource.make[F, ExecutorService](f)(es => Sync[F].delay(es.shutdownNow).void)
 
   object unsafe {
-    def unboundExecutorUnsafe[F[_]: Sync]: F[ExecutorService] = Sync[F].delay {
-      E.newCachedThreadPool(new ThreadFactory {
-        private val counter = new AtomicLong(0L)
+    def unboundExecutorUnsafe[F[_]: Sync](
+        configureThreadFactory: F[ThreadFactory] => F[ThreadFactory]): F[ExecutorService] = {
+      //delay used to avoid eager evaluation in case that instance is ignored by the configurer
+      val factoryF: F[ThreadFactory] = Sync[F].delay {
+        new ThreadFactory {
+          private val counter = new AtomicLong(0L)
 
-        def newThread(r: Runnable) = {
-          val th = new Thread(r)
-          th.setName("linebacker-thread-" + counter.getAndIncrement.toString)
-          th.setDaemon(true)
-          th
+          def newThread(r: Runnable): Thread = {
+            val th = new Thread(r)
+            th.setName("linebacker-thread-" + counter.getAndIncrement.toString)
+            th.setDaemon(true)
+            th
+          }
         }
-      })
+      }
+
+      configureThreadFactory(factoryF).flatMap { factory =>
+        Sync[F].delay {
+          E.newCachedThreadPool(factory)
+        }
+      }
     }
-    def fixedPoolExecutorUnsafe[F[_]: Sync](n: Int): F[ExecutorService] =
-      Sync[F].delay { E.newFixedThreadPool(n) }
 
-    def workStealingPoolUnsafe[F[_]: Sync](n: Int): F[ExecutorService] =
-      Sync[F].delay(E.newWorkStealingPool(n))
+    def fixedPoolExecutorUnsafe[F[_]: Sync](
+        n: Int,
+        configureThreadFactory: F[ThreadFactory] => F[ThreadFactory]): F[ExecutorService] = {
+      val factoryF = configureThreadFactory(Sync[F].delay(E.defaultThreadFactory()))
 
-    def forkJoinPoolUnsafe[F[_]: Sync](n: Int): F[ExecutorService] =
-      Sync[F].delay(new ForkJoinPool(n))
+      factoryF.flatMap { factory =>
+        Sync[F].delay { E.newFixedThreadPool(n, factory) }
+      }
+    }
+
+    def workStealingPoolUnsafe[F[_]: Sync](
+        n: Int,
+        configureThreadFactory: F[ForkJoinWorkerThreadFactory] => F[ForkJoinWorkerThreadFactory]): F[
+      ExecutorService] = {
+      val factoryF = configureThreadFactory(
+        Sync[F].point(ForkJoinPool.defaultForkJoinWorkerThreadFactory))
+
+      factoryF.flatMap { factory =>
+        Sync[F].delay {
+          new ForkJoinPool(n, factory, null, true)
+        }
+      }
+    }
+
+    def forkJoinPoolUnsafe[F[_]: Sync](
+        n: Int,
+        configureThreadFactory: F[ForkJoinWorkerThreadFactory] => F[ForkJoinWorkerThreadFactory]): F[
+      ExecutorService] = {
+      val factoryF = configureThreadFactory(
+        Sync[F].point(ForkJoinPool.defaultForkJoinWorkerThreadFactory))
+
+      factoryF.flatMap { factory =>
+        Sync[F].delay {
+          new ForkJoinPool(n, factory, null, false)
+        }
+      }
+    }
   }
 }
